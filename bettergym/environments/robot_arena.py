0fd395174c4c90d168fe3016c52b42e423a6bedc
LorenzoBonanni <lorenzopuntobonanni@gmail.com>
2023-06-26 12:13:09 +0200

    Modified RobotArena step function now distance is compute on s_t+1

--- bettergym/environments/robot_arena.py
+++ bettergym/environments/robot_arena.py
@@ -164,8 +164,8 @@ class RobotArena:
         :param action: action performed by the agent
         :return:
         """
-        self.dist_goal = dist_to_goal(self.state.x[:2], self.state.goal)
         self.state.x = self.motion(self.state.x, action)
+        self.dist_goal = dist_to_goal(self.state.x[:2], self.state.goal)
         collision = self.check_collision(self.state)
         goal = self.dist_goal <= self.config.robot_radius
         out_boundaries = self.check_out_boundaries(self.state)
@@ -179,8 +179,8 @@ class RobotArena:
         :param action: action performed by the agent
         :return:
         """
-        self.dist_goal = dist_to_goal(self.state.x[:2], self.state.goal)
         self.state.x = self.motion(self.state.x, action)
+        self.dist_goal = dist_to_goal(self.state.x[:2], self.state.goal)
         goal = self.dist_goal <= self.config.robot_radius
         out_boundaries = self.check_out_boundaries(self.state)
         collision = False

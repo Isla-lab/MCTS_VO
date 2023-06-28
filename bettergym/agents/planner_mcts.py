260527cd7fcc88fca9d9571a1e52a5616d496384
LorenzoBonanni <lorenzopuntobonanni@gmail.com>
2023-06-28 17:22:36 +0200

    Bug Fix, if terminal state add immidiate rwrd to q_val

--- bettergym/agents/planner_mcts.py
+++ bettergym/agents/planner_mcts.py
@@ -148,6 +148,7 @@ class Mcts(Planner):
             state_id = new_state_id
             if terminal or depth+1 >= self.computational_budget:
                 # self.info["rollout_values"].append(r)
+                prev_node.a_values[action_idx] += r
                 return r
             else:
                 total_rwrd = r + self.discount * self.simulate(state_id, depth + 1)

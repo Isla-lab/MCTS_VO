03cdd6736bf0e59436da71dbad60a3094eaeaedc
LorenzoBonanni <lorenzopuntobonanni@gmail.com>
2023-08-18 21:41:39 +0200

    uncommented controller

--- bettergym/agents/controller.py
+++ bettergym/agents/controller.py
@@ -37,22 +37,22 @@ class Controller:
             print(f"action: {action}")
             # TODO: Do something
 
-            # dist_minus_pi = np.abs(x[2] - -np.pi)
-            # dist_pi = np.abs(x[2] - np.pi)
-            # minimum_is_dist_minus_pi = dist_minus_pi < dist_pi
-            # action_copy[1] = dist_minus_pi + np.abs(action[1] - np.pi) if minimum_is_dist_minus_pi \
-            #     else dist_pi + np.abs(action[1] - -np.pi)
-            # action_copy[0] = np.abs(x[3] - action[0])
-            # delta = action_copy / self.reduction_factor
-            # action_copy = np.array([x[3], x[2]])
-            #
-            # action_copy[1] += -delta[1] if minimum_is_dist_minus_pi else delta[1]
-            # # if current velocity is higher than the target one(action) decrease it
-            # if x[3] > action[0]:
-            #     action_copy[0] -= delta[0]
-            # else:
-            #     action_copy[0] += delta[0]
-            # in_range = any([rr[0] <= action_copy[1] <= rr[1] for rr in robot_angles])
-            # print(f"IN RANGE: {in_range}")
-        # action_copy[1] = (action_copy[1] + np.pi) % (2 * np.pi) - np.pi
+            dist_minus_pi = np.abs(x[2] - -np.pi)
+            dist_pi = np.abs(x[2] - np.pi)
+            minimum_is_dist_minus_pi = dist_minus_pi < dist_pi
+            action_copy[1] = dist_minus_pi + np.abs(action[1] - np.pi) if minimum_is_dist_minus_pi \
+                else dist_pi + np.abs(action[1] - -np.pi)
+            action_copy[0] = np.abs(x[3] - action[0])
+            delta = action_copy / self.reduction_factor
+            action_copy = np.array([x[3], x[2]])
+
+            action_copy[1] += -delta[1] if minimum_is_dist_minus_pi else delta[1]
+            # if current velocity is higher than the target one(action) decrease it
+            if x[3] > action[0]:
+                action_copy[0] -= delta[0]
+            else:
+                action_copy[0] += delta[0]
+            in_range = any([rr[0] <= action_copy[1] <= rr[1] for rr in robot_angles])
+            print(f"IN RANGE: {in_range}")
+        action_copy[1] = (action_copy[1] + np.pi) % (2 * np.pi) - np.pi
         return action_copy
